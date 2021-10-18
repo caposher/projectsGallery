d729f3f4cd840a5a08902ef2c4bc11813d7856a9
capos <caposher@gmail.com>
2021-10-18 16:40:01 +0300

    fix alignment bug

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ function renderProjects() {
   var projects = getProjects();
   var strHTML = projects.map(function (proj, idx) {
     return `
-            <div class='col-md-4 col-sm-6 portfolio-item" data-id="${idx}'>
-                <a class='portfolio-link' data-toggle='modal' href='#portfolioModal'>
-                    <div class='portfolio-hover'>
-                        <div class='portfolio-hover-content'>
-                            <i class='fa fa-plus fa-3x'></i>
+            <div class="col-md-4 col-sm-6 portfolio-item text-center" data-id="${idx}">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <i class="fa fa-plus fa-3x"></i>
                         </div>
                     </div>
-                    <img class='rounded mx-auto d-block' src='img/portfolio/${proj.name}.jpg' alt='${proj.name}' />
+                    <img class="rounded mx-auto d-block" src="img/portfolio/${proj.name}.jpg" alt="${proj.name}" />
                 </a>
-                <div class='portfolio-caption'>
+                <div class="portfolio-caption">
                     <h4>${proj.title}</h4>
-                    <p class='text-muted'>${proj.category}</p>
+                    <p class="text-muted">${proj.category}</p>
                 </div>
             </div>`;
   });
